93ee8bf5d89d7547ffb6313e801e47527a68b13a
Subhamdas20 <subham21.sd@gmail.com>
2021-09-12 14:45:19 +0530

    Rolling the dice and checking position for option no position,snake or ladder

--- src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
+++ src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
@@ -2,21 +2,36 @@ package com.bz.snakeandladder;
 import java.util.*;
 public class SnakeAndLadderAssignment {
     static int position = 0;
-
-    void startGame() {
-        System.out.println("Position of player at start is " + position);
+    static Random random = new Random();
+    public static int getRandomNumber() {              // generating random number between 1 to 6
+        int randomNumber = random.nextInt(6) + 1;
+        return randomNumber;
     }
-    void getRandomNumber() {
-        Random random = new Random();
-        int randomNumber = 1;
-        if (randomNumber > 0) {
-            randomNumber = random.nextInt(7);
-            System.out.println(randomNumber);
+    void checkOption() {
+        for(int i=1;i<=10;i++) {//checking options for playing
+            int getRandom = getRandomNumber();
+            int randomOption = random.nextInt(3);
+            System.out.println("Number received after rolling dice is"+getRandom);
+            switch (randomOption) {
+                case 0:
+                    position = position;
+                    System.out.println("Position of player at no play is " + position);
+                    break;
+                case 1:
+                    position = position + getRandom;
+                    System.out.println("Position of player after climbing ladder is " + position);
+                    break;
+                case 2:
+                    position = position - getRandom;
+                    System.out.println("Position of player after stepping on snake  is " + position);
+                    break;
+            }
         }
     }
     public static void main(String[] args) {
+
         SnakeAndLadderAssignment object1=new SnakeAndLadderAssignment();
-        object1.startGame();
-        object1.getRandomNumber();
+        object1.checkOption();
     }
+
 }
