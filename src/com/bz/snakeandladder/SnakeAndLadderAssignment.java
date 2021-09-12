8570a1742d32cb21bfcede045ce2b14acabaf2a9
Subhamdas20 <subham21.sd@gmail.com>
2021-09-12 13:14:06 +0530

    rolling the dice to get a number between 1 to 6

--- src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
+++ src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
@@ -1,13 +1,22 @@
 package com.bz.snakeandladder;
-
+import java.util.*;
 public class SnakeAndLadderAssignment {
     static int position = 0;
 
     void startGame() {
         System.out.println("Position of player at start is " + position);
     }
+    void getRandomNumber() {
+        Random random = new Random();
+        int randomNumber = 1;
+        if (randomNumber > 0) {
+            randomNumber = random.nextInt(7);
+            System.out.println(randomNumber);
+        }
+    }
     public static void main(String[] args) {
         SnakeAndLadderAssignment object1=new SnakeAndLadderAssignment();
         object1.startGame();
+        object1.getRandomNumber();
     }
 }
