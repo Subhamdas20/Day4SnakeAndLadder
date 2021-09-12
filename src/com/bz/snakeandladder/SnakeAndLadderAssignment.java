8e1d2ab697948363daa993ad8683d359a798172a
Subhamdas20 <subham21.sd@gmail.com>
2021-09-12 16:02:42 +0530

    printing win statement if position becomes 100;if position becomes greater than 100 than position returns to previous value

--- src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
+++ src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
@@ -8,30 +8,45 @@ public class SnakeAndLadderAssignment {
         return randomNumber;
     }
     void checkOption() {
-            while(position<=100){
-            int getRandom = getRandomNumber();
-            int randomOption = random.nextInt(3);
-            System.out.println("Number received after rolling dice is"+getRandom);
-            switch (randomOption) {
-                case 0:
-                    position = position;
-                    System.out.println("Position of player at no play is " + position);
-                    break;
-                case 1:
-                    position = position + getRandom;
-                    System.out.println("Position of player after climbing ladder is " + position);
-                    break;
-                case 2:
-                    if (position - getRandom <= 0) {
-                        position = 0;
-                    } else {
-                        position = position - getRandom;
-                        System.out.println("Position of player after stepping on snake  is " + position);
+                while(position<=100){
+                    int getRandom = getRandomNumber();
+                    int randomOption = random.nextInt(3);      // // generating random options for play
+//                System.out.println("Number received after rolling dice is"+getRandom);
+
+                    switch (randomOption) {                //for printing positions at no play,ladder and snake
+                        case 0:
+                            position = position;
+                            System.out.println("Position of player at no play is " + position);
+                            break;
+                        case 1:
+                            position = position + getRandom;
+                            if(position<=100)
+                                System.out.println("Position of player after climbing ladder is " + position);
+                            else
+                                System.out.println("Position of player after climbing ladder is " + (position-getRandom));
+                            break;
+                        case 2:
+                            if (position - getRandom <= 0) {
+                                position = 0;
+                            } else {
+                                position = position - getRandom;
+                                System.out.println("Position of player after stepping on snake  is " + position);
+                            }
+                            break;
                     }
-                    break;
+                    if (position > 100) {                                //if position is greater than 100 it will roll back to previous value
+                        position = position - getRandom;
+                    } else if (position == 100) {
+                        System.out.println("Player wins");
+                        break;
+                    } else {
+                        position=position;
+                    }
                 }
-            }
+
+
     }
+
 
     public static void main(String[] args) {
 
