cdde0abc13ebe3a423c81e27560cafdbb377a129
Subhamdas20 <subham21.sd@gmail.com>
2021-09-12 13:07:06 +0530

    First commit

---
+++ src/com/bz/snakeandladder/SnakeAndLadderAssignment.java
@@ -0,0 +1,13 @@
+package com.bz.snakeandladder;
+
+public class SnakeAndLadderAssignment {
+    static int position = 0;
+
+    static void startGame() {
+        System.out.println("Position of player at start is " + position);
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
